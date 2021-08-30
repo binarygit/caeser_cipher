f37d3711e72be42f0d9ee3c2d62ccedfd78b86cb
binarygit <Inscription.abhi@outlook.com>
2021-08-30 12:46:37 +0545

    program can encode message satisfactorily

--- lib/main.rb
+++ lib/main.rb
@@ -3,12 +3,38 @@ require 'pry-byebug'
 
 class CaeserCipher
   def encode(message, shift)
-    if message == 'z'
-      'e'
+    message.split('').map do |character|
+      if alphabet?(character)
+        encoded_character = shifted_ascii_value(ascii_value(character), shift).chr
+        encoded_character.capitalize! if capital?(character)
+        encoded_character
+      else
+        character
+      end
+    end
+           .join
+  end
+
+  def shifted_ascii_value(ascii_value, shift = 0)
+    potential_ascii_value = ascii_value + shift
+
+    if (97..122).include?(potential_ascii_value)
+      potential_ascii_value
     else
-      (message.ord + shift).chr
+      potential_ascii_value = (potential_ascii_value - 122) + 96
+      shifted_ascii_value(potential_ascii_value)
     end
   end
+
+  def ascii_value(character)
+    character.downcase.ord
+  end
+
+  def capital?(character)
+    character.match?(/[A-Z]/)
+  end
+
+  def alphabet?(character)
+    character.match?(/^[A-Z]+$/i)
+  end
 end
-
-
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -5,17 +5,48 @@ RSpec.describe CaeserCipher do
 
   describe '#encode' do
 
-    it 'encodes a letter' do
+    it 'encodes a single letter' do
       expect(subject.encode('b', 3)).to eql('e')
     end
 
-    it 'encodes another letter' do
-      expect(subject.encode('a', 1)).to eql('b')
+    it 'encodes another single letter' do
+      expect(subject.encode('x', 4)).to eql('b')
     end
 
-    it 'encodes yet another letter' do
-      expect(subject.encode('z', 5)).to eql('e')
+    it 'encodes all english alphabets' do
+      expect(subject.encode('abcdefghijklmnopqrstuvwxyz', 5)).to eql('fghijklmnopqrstuvwxyzabcde')
     end
 
+    it 'encodes a capital letter while preserving case' do
+      expect(subject.encode('B', 3)).to eql('E')
+    end
+
+    it 'encodes another capital letter while preserving case' do
+      expect(subject.encode('X', 3)).to eql('A')
+    end
+
+    it 'encodes all capital english alphabets' do
+      expect(subject.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 5)).to eql('FGHIJKLMNOPQRSTUVWXYZABCDE')
+    end
+
+    it 'doesnot encode non-alphabet letters' do
+      expect(subject.encode('!12@&!', 3)).to eql('!12@&!')
+    end
+
+    it 'encodes a sentence' do
+      expect(subject.encode('rafting is fun', 2)).to eql('tchvkpi ku hwp') 
+    end
+
+    it 'encodes a sentence with punctuation' do
+      expect(subject.encode('hello, this is fun is it not?', 12)).to eql('tqxxa, ftue ue rgz ue uf zaf?')
+    end
+
+    it 'encodes a sentence with a high shift value' do
+      expect(subject.encode('can YOU encode THIS???', 155)).to eql('bzm XNT dmbncd SGHR???')
+    end
+
+    it 'encodes a sentence with even higher shift value' do
+      expect(subject.encode('can YOU encode THIS???', 1550)).to eql('sqd OEK udsetu JXYI???')
+    end
   end
 end
